3cc2c609fe5150555912e1da23c9a01edf2b3741
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-13 10:38:07 +0530

    commited in UC5

--- Tasks/Tasks/Employeewage.cs
+++ Tasks/Tasks/Employeewage.cs
@@ -12,28 +12,34 @@ namespace Tasks
         public const int Is_part_time = 1;
         public const int Is_full_time = 2;
         public const int emp_rate_per_hour = 20;
+        public const int num_of_working_days = 2;
         static void Main(string[] args)
         {
-            
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
 
-            int empCheck = random.Next(0, 3);
-            switch (empCheck) {
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
 
-                case Is_part_time:
-                    empHrs = 4;
-                    break;
-                case Is_full_time:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+            for (int day = 0; day < num_of_working_days; day++)
+            {
+                Random random = new Random();
+
+                int empCheck = random.Next(0, 3);
+                switch (empCheck) {
+
+                    case Is_part_time:
+                        empHrs = 4;
+                        break;
+                    case Is_full_time:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * emp_rate_per_hour;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHrs * emp_rate_per_hour;
-            Console.WriteLine("Emp Wage : "+empWage);
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
 }
