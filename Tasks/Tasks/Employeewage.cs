2ec1054fb516f1e88c58e785ba239cf0f7624a05
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-13 09:52:43 +0530

    Commited UC1

---
+++ Tasks/Tasks/Employeewage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Employeewage
+    {
+        static void Main(string[] args)
+        {
+            int Is_Full_Time = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Is_Full_Time) 
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+            }
+        }
+    }
+}
