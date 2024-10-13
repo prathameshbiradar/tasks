f291efffda8f99c52c89426e30a817e95795c5e3
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-13 12:12:08 +0530

    Commited UC7

--- Tasks/Tasks/Employeewage.cs
+++ Tasks/Tasks/Employeewage.cs
@@ -14,33 +14,42 @@ namespace Tasks
         public const int emp_rate_per_hour = 20;
         public const int num_of_working_days = 2;
         public const int max_hrs_in_month = 10;
-        static void Main(string[] args)
+
+        public static int computeEmpWage()
         {
 
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
             while (totalEmpHrs <= max_hrs_in_month && totalWorkingDays < num_of_working_days)
-            { 
+            {
+
                 totalWorkingDays++;
                 Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck) {
-
-                    case Is_part_time:
+                int empCheck = random.Next(0,3);
+                switch (empCheck)
+                { 
+                    case Is_part_time :
                         empHrs = 4;
                         break;
-                    case Is_full_time:
+                    case Is_full_time :
                         empHrs = 8;
                         break;
                     default:
-                        empHrs = 0;
+                        empHrs = 0; 
                         break;
                 }
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day#:"+ totalWorkingDays+" Emp Hrs : "+empHrs);
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
             }
             int totalEmpWage = totalEmpHrs * emp_rate_per_hour;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            return totalEmpWage;
+        }
+
+        static void Main(string[] args)
+        {
+
+           computeEmpWage();
         }
     }
 }
